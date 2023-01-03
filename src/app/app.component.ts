17e1b7a4e93b4421cb706efe15ea7ec575bdaa32
jaydip-tagline <jaydiph.tagline@gmail.com>
2023-01-03 15:45:31 +0530

    Generate template-driven-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-forms';
+  @ViewChild('myForm') myForm = 'ngForm';
+  public firstname!: string;
+  public lastname!: string;
+  public emailData!: string;
+  initialVal!: null;
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.log('form :>> ', this.myForm);
+    // this.firstname = form.value.firstname;
+    // this.lastname = form.value.lastname;
+    // this.emailData = form.value.email;
+  }
+
+  // resetUserForm() {
+  //   this.myForm.resetForm();
+  //   this.initialVal = null;
+  // }
 }
