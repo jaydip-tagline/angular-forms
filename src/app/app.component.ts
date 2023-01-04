f0634efda1724f56a48d31abe2b898db0432bec6
jaydip-tagline <jaydiph.tagline@gmail.com>
2023-01-04 15:01:53 +0530

    Implement CRUD in Template Driven Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,15 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+export interface DataList {
+  id: number;
+  firstname: string;
+  lastname: string;
+  gender: string;
+  mobile: string;
+  email: string;
+  fData?: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,24 +17,64 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   title = 'angular-forms';
-  @ViewChild('myForm') myForm = 'ngForm';
-  public firstname!: string;
-  public lastname!: string;
-  public emailData!: string;
-  initialVal!: null;
-  constructor() {}
+  private userId!: number;
+  @ViewChild('myForm') myForm!: NgForm;
 
+  userData: DataList[] = [
+    {
+      id: 1,
+      firstname: 'User 1',
+      lastname: 'demo',
+      gender: 'Male',
+      mobile: '1234567890',
+      email: 'demo1@gmail.com',
+    },
+    {
+      id: 2,
+      firstname: 'User 2',
+      lastname: 'demo',
+      gender: 'Male',
+      mobile: '1234567890',
+      email: 'demo2@gmail.com',
+    },
+  ];
+
+  isUpdate: boolean = false;
+
+  constructor() {}
   ngOnInit(): void {}
 
   onSubmit() {
-    console.log('form :>> ', this.myForm);
-    // this.firstname = form.value.firstname;
-    // this.lastname = form.value.lastname;
-    // this.emailData = form.value.email;
+    if (this.myForm.invalid) {
+      return;
+    } else {
+      if (this.userId) {
+        const index = this.userData.findIndex(
+          (res: any) => res.id === this.userId
+        );
+        this.userData[index] = {
+          id: this.userId,
+          ...this.myForm.value,
+        };
+        this.isUpdate = false;
+      } else {
+        const data = {
+          id: this.userData.length + 1,
+          ...this.myForm.value,
+        };
+        this.userData.push(data);
+      }
+      this.myForm.reset();
+    }
   }
 
-  // resetUserForm() {
-  //   this.myForm.resetForm();
-  //   this.initialVal = null;
-  // }
+  delete(data: number): any {
+    this.userData.splice(data, 1);
+  }
+
+  edit(data: DataList) {
+    this.userId = data.id;
+    this.myForm.form.patchValue(data);
+    this.isUpdate = true;
+  }
 }
